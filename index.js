2fcd786739a854340db0a6e4e87a86b49d35468d
N Jayant Rao <njayantrao@gmail.com>
2026-04-14 16:40:01 +0530

    added health check route

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/", async (req, res) => {
   res.send("Server up n running...");
 });
 
+app.get("/health", async (req, res) => {
+  res.send("Server is healthy...");
+});
+
 app.listen(port, () => {
   console.log(`server running on port ${port}...`);
 });
